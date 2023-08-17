bf5c3df2ca5c5fc4367bc249264ab763a34a9c91
Levi-Ten <porango100@gmail.com>
2023-08-17 13:54:24 +0300

    sending the draft project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    Welcome to the new project! We wish you success in achieving it!
+  </React.StrictMode>
+);
