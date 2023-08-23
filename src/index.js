d0fcfaa961e62065d059a170a562e508547bb850
Irina <131261791+IrinaGatalaiciuc@users.noreply.github.com>
2023-08-23 13:47:37 +0300

    create app file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./app/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    Welcome to the new project! We wish you success in achieving it!
+   <App />
   </React.StrictMode>
 );
